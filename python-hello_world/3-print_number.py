4cb909fe8ae886d9fb3283f2bca106cc297e7c43
Samenergy <s.dushime@alustudent.com>
2023-07-06 04:32:31 -0700

    3-print_number.py

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-negative_number = -98
-positive_number = 333
-print(f"{negative_number} Battery street\n")
-print(f"{positive_number} Battery street\n")
+number = 333
+print(f"{number} Battery street\n")
