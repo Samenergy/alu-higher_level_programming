0972af8918f5d128ff2f8c96739a809297e4085d
Samenergy <s.dushime@alustudent.com>
2023-07-07 04:49:13 -0700

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i == 0:
-            print("{:01d}, ".format(j), end="")
+            print("{:02d}, ".format(j), end="")
         else:
             print("{:01d}{:01d}, ".format(i, j), end="")
 print()
