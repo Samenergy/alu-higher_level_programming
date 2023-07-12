d28d8084f56db9002946e5def063b104f2914bcf
Samenergy <s.dushime@alustudent.com>
2023-07-12 06:57:18 -0700

    python-data_structures

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    seen_nums = set()
     for num in my_list:
-        if num not in seen_nums:
-            print("{:d}".format(num))
-            seen_nums.add(num)
+        print("{:d}".format(num))
 my_list = [1, 2, 3]
 print_list_integer(my_list)
+
