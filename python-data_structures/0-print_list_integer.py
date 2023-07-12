f06c88a4f4800ae18cc94aabbfebb1521408890c
Samenergy <s.dushime@alustudent.com>
2023-07-12 06:45:40 -0700

    python-data_structures

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for num in my_list:
+    unique_nums = set(my_list)
+    for num in unique_nums:
         print("{:d}".format(num))
-my_list = [1, 2, 3, 4, 5]
+my_list = [1, 2, 3, 4, 5, 1, 2, 3]
 print_list_integer(my_list)
