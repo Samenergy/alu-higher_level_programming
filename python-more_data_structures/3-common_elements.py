30a5e62d6f1f5257319016878bcaa85469b2b7b7
Samenergy <s.dushime@alustudent.com>
2023-07-14 13:25:31 -0700

    python-more_data_structures

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    unique_nums = set()
-    sum_unique = 0
-    for num in my_list:
-        if num not in unique_nums:
-            sum_unique += num
-            unique_nums.add(num)
-    return sum_unique
+def common_elements(set_1, set_2):
+    return (set_1 & set_2)
