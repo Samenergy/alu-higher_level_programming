77e51b9b00f555a61302cb51508007aefc803cad
Samenergy <s.dushime@alustudent.com>
2023-07-24 03:18:41 -0700

    python-classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 class square:
+    """
+    A class representing a square.
 
+    Attributes:
+        side_length (float): The length of the sides of the square.
+    """
     pass
