226fde1f7578ac3325969118f3a6ced81434418f
Samuel Dushime <127239702+Samenergy@users.noreply.github.com>
2023-08-03 13:39:28 +0200

    python-network_0

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-url="$1"
-
-# Send request and store the response in a temporary file
-response=$(mktemp)
-curl -s "$url" -o "$response"
-
-# Get the size of the response body in bytes
-size=$(stat -c %s "$response")
-
-# Display the size of the response body
-echo "Size of the response body: $size bytes"
-
-# Clean up temporary files
-rm "$response"
-
+# Send the request using curl, and output the size of the response body in bytes
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
